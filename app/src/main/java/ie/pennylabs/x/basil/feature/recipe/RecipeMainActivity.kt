6f9dd8d9d2b5dfe184944b105e29536b42ebbcf9
Elliot Tormey <elliot.tormey@gmail.com>
2018-05-19 21:53:14 +0100

    add bottom sheet tabs for recipe detail

--- app/src/main/java/ie/pennylabs/x/basil/feature/recipe/RecipeMainActivity.kt
+++ app/src/main/java/ie/pennylabs/x/basil/feature/recipe/RecipeMainActivity.kt
@@ -1,10 +1,14 @@
 package ie.pennylabs.x.basil.feature.recipe
 
 import android.os.Bundle
+import android.support.design.widget.BottomSheetBehavior
 import android.support.v7.app.AppCompatActivity
 import ie.pennylabs.x.basil.R
+import kotlinx.android.synthetic.main.activity_recipe_main.*
 
 class RecipeMainActivity : AppCompatActivity() {
+  private val bottomSheet by lazy { BottomSheetBehavior.from(bsRecipeDetail) }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_recipe_main)
