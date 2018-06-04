693147501dfcd3a1634351f473680badbe8565cb
Elliot Tormey <elliot.tormey@gmail.com>
2018-06-04 14:53:04 +0100

    update DirectionsPage to use merge tag on layout

--- app/src/main/java/ie/pennylabs/x/basil/feature/recipe/detail/directions/DirectionsPage.kt
+++ app/src/main/java/ie/pennylabs/x/basil/feature/recipe/detail/directions/DirectionsPage.kt
@@ -2,10 +2,10 @@ package ie.pennylabs.x.basil.feature.recipe.detail.directions
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.widget.FrameLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import ie.pennylabs.x.basil.R
 
-class DirectionsPage(context: Context) : FrameLayout(context) {
+class DirectionsPage(context: Context) : ConstraintLayout(context) {
   init {
     LayoutInflater.from(context).inflate(R.layout.page_directions, this, true)
   }
