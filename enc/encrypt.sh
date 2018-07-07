b61f01dea966782f3401a6046b241d87c0849f77
Elliot Tormey <elliot.tormey@gmail.com>
2018-07-07 16:18:05 +0100

    move KeyStore.kt to basil package

--- enc/encrypt.sh
+++ enc/encrypt.sh
@@ -25,7 +25,7 @@ checkEnv() {
 checkEnv PENNY_ENCRYPT_KEY
 
 echo "Encrypting files"
-openssl aes-256-cbc -a -md sha256 -in buildSrc/src/main/java/KeyStore.kt -out enc/KeyStore.kt.aes -k $PENNY_ENCRYPT_KEY
+openssl aes-256-cbc -a -md sha256 -in buildSrc/src/main/java/basil/KeyStore.kt -out enc/KeyStore.kt.aes -k $PENNY_ENCRYPT_KEY
 openssl aes-256-cbc -a -md sha256 -in signing/release.keystore -out enc/release.keystore.aes -k $PENNY_ENCRYPT_KEY
 openssl aes-256-cbc -a -md sha256 -in signing/play.json -out enc/play.json.aes -k $PENNY_ENCRYPT_KEY
 log "Files encrypted"
